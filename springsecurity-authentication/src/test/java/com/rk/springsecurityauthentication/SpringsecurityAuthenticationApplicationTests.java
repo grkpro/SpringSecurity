f5699865db096c552a81f73bc16d414c633e61af
Ravi Kumar Gowri <ravikumar.gowri@tavant.com>
2021-03-29 17:55:46 +0530

    Spring Security

---
+++ springsecurity-authentication/src/test/java/com/rk/springsecurityauthentication/SpringsecurityAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rk.springsecurityauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringsecurityAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
